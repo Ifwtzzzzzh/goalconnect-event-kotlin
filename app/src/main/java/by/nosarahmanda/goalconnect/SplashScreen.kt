3f34a8ddd42a6aa347e6052aa93a16924e8ce2ff
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-12-08 12:12:07 +0700

    feat: add splash screen

--- app/src/main/java/by/nosarahmanda/goalconnect/SplashScreen.kt
+++ app/src/main/java/by/nosarahmanda/goalconnect/SplashScreen.kt
@@ -1,42 +1,23 @@
 package by.nosarahmanda.goalconnect
 
-import android.animation.ObjectAnimator
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
-import android.view.animation.AnticipateInterpolator
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.S)
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            goToSplashScreenActivity()
-        }, 3000L)
-    }
-
-    private fun goToSplashScreenActivity() {
-        Intent(this, SplashScreen::class.java).also {
-            startActivity(it)
-            finish()
         }
     }
 }
