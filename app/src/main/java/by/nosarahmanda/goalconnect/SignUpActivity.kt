f71f3a29fe8662cbea00c01fb8578ac24624fd15
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-12-08 13:49:59 +0700

    style: styling sign up page

--- app/src/main/java/by/nosarahmanda/goalconnect/SignUpActivity.kt
+++ app/src/main/java/by/nosarahmanda/goalconnect/SignUpActivity.kt
@@ -1,19 +1,16 @@
 package by.nosarahmanda.goalconnect
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SplashScreen : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/by/nosarahmanda/goalconnect/SplashScreenActivity.kt
+++ app/src/main/java/by/nosarahmanda/goalconnect/SplashScreenActivity.kt
@@ -1,37 +1,28 @@
 package by.nosarahmanda.goalconnect
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.S)
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            goToSplashScreenActivity()
-        }, 3000L)
-    }
-
-    private fun goToSplashScreenActivity() {
-        Intent(this, SplashScreenActivity::class.java).also {
-            startActivity(it)
-            finish()
+        findViewById<ImageButton>(R.id.btn_get_started).setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
